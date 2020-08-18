4829a189cb44f51b7b3ab11d18c75d484259e9f8
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-08-18 16:46:49 +0200

    added bubble_sort_by method

--- bubble_sorting.rb
+++ bubble_sorting.rb
@@ -5,7 +5,6 @@ def bubble_sort(arr)
     arr.each_with_index do |_val, index|
       break if index + 1 == arr.length
 
-      # puts "val is #{val} index is #{arr[index]}"
       arr[index] > arr[index.next] && (arr[index], arr[index.next] = arr[index.next], arr[index])
     end
     arr
@@ -13,3 +12,19 @@ def bubble_sort(arr)
 end
 
 puts bubble_sort([3, 0, 128 - 3, -65, 45, 4, 5, 1, 10, 2])
+
+def bubble_sort_by(arr)
+  arr.each_with_index do |_value, _i|
+    arr.each_with_index do |_val, index|
+      break if index.next == arr.length
+
+      difference = yield(arr[index], arr[index.next])
+      difference.positive? && (arr[index], arr[index.next] = arr[index.next], arr[index])
+    end
+  end
+  puts arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
