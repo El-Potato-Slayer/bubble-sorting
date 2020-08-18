7bdaaea72916c26f145bcfeb1563bf848a951983
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-08-18 18:16:36 +0400

    Fix bubble sorting algorithm

--- bubble_sorting.rb
+++ bubble_sorting.rb
@@ -1,25 +1,15 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   arr.each_with_index do |_value, _i|
-    arr.each_with_index do |val, index|
-      if index+1 == arr.length
-        break
-      end
-      if val > arr[index.next]
-        puts val.to_s + " " + arr[index.next].to_s
-        val, arr[index.next] = arr[index.next], val
-      end
-    end
-  end
+    arr.each_with_index do |_val, index|
+      break if index + 1 == arr.length
 
-  # for i in 0..._arr.length-1
-  #   for j in 0..._arr.length-1
-  #     if _arr[j] > _arr[j.next]
-  #        _arr[j], _arr[j.next] = _arr[j.next], _arr[j]
-  #     end
-  #   end
-  # end
-  # _arr
-  # arr
+      # puts "val is #{val} index is #{arr[index]}"
+      arr[index] > arr[index.next] && (arr[index], arr[index.next] = arr[index.next], arr[index])
+    end
+    arr
+  end
 end
 
-puts bubble_sort([3, 4, 5, 1, 10, 2])
+puts bubble_sort([3, 0, 128 - 3, -65, 45, 4, 5, 1, 10, 2])
